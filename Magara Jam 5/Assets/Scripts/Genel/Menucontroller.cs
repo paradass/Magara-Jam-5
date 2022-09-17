bc934351028d13de50db64edb8373c5e479b6bc4
kazilezora <69401029+kazilezora@users.noreply.github.com>
2022-09-17 19:01:11 +0300

    Menu

--- Magara Jam 5/Assets/Scripts/Genel/Menucontroller.cs
+++ Magara Jam 5/Assets/Scripts/Genel/Menucontroller.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Menucontroller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Oyunbasla()
     {
-        
+        SceneManager.LoadScene(1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void cýkýs()
     {
-        
+        Application.Quit();
     }
 }
