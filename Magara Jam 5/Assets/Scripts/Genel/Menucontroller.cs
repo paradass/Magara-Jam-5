1bb388c3b1e05454634174c942bd25f2102a12e8
kazilezora <69401029+kazilezora@users.noreply.github.com>
2022-09-17 22:54:05 +0300

    map2

--- Magara Jam 5/Assets/Scripts/Genel/Menucontroller.cs
+++ Magara Jam 5/Assets/Scripts/Genel/Menucontroller.cs
@@ -5,12 +5,15 @@ using UnityEngine.SceneManagement;
 public class Menucontroller : MonoBehaviour
 {
     public AudioSource ad;
+
     public void Oyunbasla()
     {
         SceneManager.LoadScene(1);
+        ad.Play();
     }
     public void cýkýs()
     {
         Application.Quit();
+        ad.Play();
     }
 }
