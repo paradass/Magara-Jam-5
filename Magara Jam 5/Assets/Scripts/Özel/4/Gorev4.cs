68ac830962a336aea06b2a9d209f9108ad63a6b9
Burak Görez <77239106+paradass@users.noreply.github.com>
2022-09-18 05:30:53 +0300

    Sahne 4 son

--- Magara Jam 5/Assets/Scripts/Özel/4/Gorev4.cs
+++ Magara Jam 5/Assets/Scripts/Özel/4/Gorev4.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Kino;
 
 public class Gorev4 : GorevSistemi
 {
     [Space(5)]
-    [SerializeField] private GameObject geyikKafasi;
+    [SerializeField] private Sprite kotuUi;
+    [SerializeField] private GameObject geyik,yaratik,geyikKafasi;
     [SerializeField] private Vector3 hedef;
     bool devamEt;
 
@@ -21,7 +23,7 @@ public class Gorev4 : GorevSistemi
     }
     void SonrakiSahne()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(5);
     }
     void GorevKontrol()
     {
@@ -29,6 +31,9 @@ public class Gorev4 : GorevSistemi
         {
             if (karakter.baltaVarmi)
             {
+                CancelInvoke("Diyalog");
+                YaziKapat(true, true);
+                StartCoroutine(JumpScare());
                 tekrarla = true;
                 gorev++;
                 return;
@@ -39,9 +44,9 @@ public class Gorev4 : GorevSistemi
         }
         if (gorev == 1 && devamEt)
         {
-            if (karakter.geyikSayisi > 0)
+            float mesafe = Vector3.Distance(karakter.transform.position, hedef);
+            if (mesafe < 5)
             {
-                CancelInvoke("GeyikOldurmeme");
                 Perde.Instance.Karart();
                 Invoke("SonrakiSahne", 2);
                 tekrarla = true;
@@ -49,33 +54,73 @@ public class Gorev4 : GorevSistemi
                 return;
             }
             if (!tekrarla) return;
-            CancelInvoke("Diyalog");
-            diyalog = 3;
-            Invoke("Diyalog", 0.1f);
-            Invoke("GeyikOldurmeme", 9);
+            diyalog = 5;
+            Invoke("Diyalog", 1f);
             tekrarla = false;
         }
     }
-
-    void GeyikOldurmeme()
-    {
-        if (gorev > 1) return;
-        karakter.baltaVarmi = false;
-        CancelInvoke("Diyalog");
-        diyalog = 6;
-        Invoke("Diyalog", 0.1f);
-        StartCoroutine(JumpScare());
-    }
     IEnumerator JumpScare()
     {
-        //transform.GetChild(0).GetComponent<AudioSource>().Stop();
+        //transform.GetChild(1).GetComponent<AudioSource>().Play();
+        //geyikKafasi.SetActive(true);
+        transform.GetChild(0).GetComponent<AudioSource>().Pause();
         yield return new WaitForSeconds(2);
-        geyikKafasi.SetActive(true);
+        //glitch
+        transform.GetChild(2).GetComponent<AudioSource>().Play();
+        Camera.main.GetComponent<DigitalGlitch>().intensity = 0.2f;
+        Camera.main.GetComponent<AnalogGlitch>().scanLineJitter = 0.23f;
+        Camera.main.GetComponent<AnalogGlitch>().verticalJump = 0.06f;
+        Camera.main.GetComponent<AnalogGlitch>().horizontalShake = 0.06f;
+        Camera.main.GetComponent<AnalogGlitch>().colorDrift = 0.2f;
+        yield return new WaitForSeconds(2);
+        //ekran temiz
+        GameObject.Find("Odun Toplama").GetComponent<Image>().sprite = kotuUi;
+        Camera.main.GetComponent<DigitalGlitch>().intensity = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().scanLineJitter = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().verticalJump = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().horizontalShake = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().colorDrift = 0f;
+        yield return new WaitForSeconds(2);
+        //glitch
+        GameObject[] agacalar = GameObject.FindGameObjectsWithTag("Agac");
+        foreach(GameObject obje in agacalar)
+        {
+            Instantiate(geyik, obje.transform.position, Quaternion.identity);
+            Destroy(obje);
+        }
+        transform.GetChild(2).GetComponent<AudioSource>().Play();
+        Camera.main.GetComponent<DigitalGlitch>().intensity = 0.2f;
+        Camera.main.GetComponent<AnalogGlitch>().scanLineJitter = 0.23f;
+        Camera.main.GetComponent<AnalogGlitch>().verticalJump = 0.06f;
+        Camera.main.GetComponent<AnalogGlitch>().horizontalShake = 0.06f;
+        Camera.main.GetComponent<AnalogGlitch>().colorDrift = 0.2f;
+        yield return new WaitForSeconds(2);
+        //ekran temiz
+        Camera.main.GetComponent<DigitalGlitch>().intensity = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().scanLineJitter = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().verticalJump = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().horizontalShake = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().colorDrift = 0f;
+        yield return new WaitForSeconds(10);
+        Destroy(Instantiate(yaratik, new Vector3(karakter.transform.position.x - 3, karakter.transform.position.y, -6), Quaternion.identity), 2);
+        yield return new WaitForSeconds(2);
         transform.GetChild(1).GetComponent<AudioSource>().Play();
+        geyikKafasi.SetActive(true);
         Camera.main.GetComponent<DigitalGlitch>().intensity = 0.6f;
         Camera.main.GetComponent<AnalogGlitch>().scanLineJitter = 0.63f;
         Camera.main.GetComponent<AnalogGlitch>().verticalJump = 0.12f;
         Camera.main.GetComponent<AnalogGlitch>().horizontalShake = 0.12f;
         Camera.main.GetComponent<AnalogGlitch>().colorDrift = 0.5f;
+
+        yield return new WaitForSeconds(2);
+        geyikKafasi.SetActive(false);
+        Camera.main.GetComponent<DigitalGlitch>().intensity = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().scanLineJitter = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().verticalJump = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().horizontalShake = 0f;
+        Camera.main.GetComponent<AnalogGlitch>().colorDrift = 0f;
+        yield return new WaitForSeconds(1);
+        devamEt = true;
+
     }
 }
