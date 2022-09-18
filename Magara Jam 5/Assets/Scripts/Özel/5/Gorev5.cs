9e19f237bfc76d2829b62067a58353d9df417b45
Burak Görez <77239106+paradass@users.noreply.github.com>
2022-09-18 18:19:52 +0300

    Proje revertten sonra

---
+++ Magara Jam 5/Assets/Scripts/Özel/5/Gorev5.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Kino;
+public class Gorev5 : GorevSistemi
+{
+    [Space(5)]
+    [SerializeField] Vector3 hedef;
+    [SerializeField] GameObject[] hayaletler;
+    public bool golgelendi;
+    public void Start()
+    {
+        base.Start();
+    }
+    void Update()
+    {
+        GorevKontrol();
+    }
+    void SonrakiSahne()
+    {
+        SceneManager.LoadScene(2);
+    }
+    void GorevKontrol()
+    {
+        if (gorev == 0)
+        {
+            float mesafe = Vector3.Distance(karakter.transform.position, hedef);
+            if (mesafe < 3)
+            {
+                tekrarla = true;
+                gorev++;
+                return;
+            }
+            if (!tekrarla) return;
+            Invoke("Diyalog", 1);
+            tekrarla = false;
+
+        }
+        if (gorev == 1)
+        {
+            if (golgelendi)
+            {
+                tekrarla = true;
+                gorev++;
+                diyalog = 7;
+                Invoke("Diyalog", 0.1f);
+                return;
+            }
+            if (!tekrarla) return;
+            CancelInvoke("Diyalog");
+            Invoke("GolgeCikar", 1);
+            tekrarla = false;
+        }
+    }
+
+    void GolgeCikar()
+    {
+        hayaletler[0].SetActive(true);
+
+    }
+}
